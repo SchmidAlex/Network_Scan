093f0f06e507bf2ac05a8c098d32e94fac0bee07
SchmidAlex <91736634+SchmidAlex@users.noreply.github.com>
2022-10-18 07:20:49 +0200

    hopefully i can count the results

--- scan.py
+++ scan.py
@@ -237,7 +237,15 @@ def compare(newDirectory, oldDirectory):
     for host in newTreeFinding:
         for oldHost in oldTreeFinding:
             if host['host'] == oldHost['host']:
-                print("its a match")
+                # We found the same host in the new scan as in the old
+                for port in host[host['host']]:
+                    for oldPort in oldHost[oldHost['host']]:
+                        if oldPort.count(port['port']) > 0:
+                            print("port exists")
+                        # if port['port'] == oldPort['port'] and port['protocol'] == oldPort['protocol']:
+                        #     print("same ports, dont do anything")
+                        # else:
+                        #     print("well it comes for every port that doesnt match... gota check in a different way :/")
         
 
 
